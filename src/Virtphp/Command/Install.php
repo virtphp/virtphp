26d0263c2674417c943d7b36ec9db1001af3acdd
Jacques Woodcock <jacques@kitewebconsulting.com>
2013-05-04 01:08:04 -0500

    Adding environment name to arguments and fixing commenting

--- src/Virtphp/Command/Install.php
+++ src/Virtphp/Command/Install.php
@@ -22,23 +22,50 @@ use Symfony\Component\Console\Output\OutputInterface;
 class Install extends Command
 {
 
+    /**
+     * Function that defines command name and what
+     * variables we are taking.
+     */
     protected function configure()
     {
         $this
             ->setName('install')
             ->setDescription('Create new virtphp from scratch.')
+            ->addArgument(
+                'name',
+                InputArgument::OPTIONAL,
+                'What is the name of your environment'
+            );
     }
 
-    /* Function to process input options for command.
+    /*
+     * Function to process input options for command.
      *
      * @param string $input
      * @param string $output
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $env_name = $input->getArgument('name');
+
+        // Check to make sure environment name is valid
+        if ($env_name && !validName($env_name)) {
+            $output->writeln('The name provided is not valid.');
+            return false;
+        }
         // Check default locations for valid PHP
         //
         // Setup environment
     }
 
+    /** 
+     * Function to make sure provided 
+     * environment name is valid.
+     *
+     * @param string $env_name
+     */
+    function validName($env_name)
+    {
+        // Logic for validating name
+    }
 }
