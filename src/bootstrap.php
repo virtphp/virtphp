f1391646639411df4625ebbd0d2f0f958cd85149
Jacques Woodcock <jacques@kitewebconsulting.com>
2013-05-03 23:32:00 -0500

    Adding bootstrap file

---
+++ src/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+function includeIfExists($file)
+{
+    if (file_exists($file)) {
+        return include $file;
+    }
+}
+
+if ((!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php')) && (!$loader = includeIfExists(__DIR__.'/../../../autoload.php'))) {
+    echo 'Error message goes here';
+    exit(1);
+}
+
+return $loader;
