45a5d53fd050bb0a424c97c6c0b98f28402f9673
Jacques Woodcock <jacques@kitewebconsulting.com>
2013-05-03 23:59:37 -0500

    Added structure for install command

--- src/Virtphp/Command/Install.php
+++ src/Virtphp/Command/Install.php
@@ -13,8 +13,32 @@
 
 namespace Virtphp\Command;
 
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+class Install extends Command
+{
 
+    protected function configure()
+    {
+        $this
+            ->setName('install')
+            ->setDescription('Create new virtphp from scratch.')
+    }
+
+    /* Function to process input options for command.
+     *
+     * @param string $input
+     * @param string $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // Check default locations for valid PHP
+        //
+        // Setup environment
+    }
+
+}
