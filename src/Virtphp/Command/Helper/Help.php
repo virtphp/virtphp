1e1719af6b1dffc927c91fb25aa1cc9f9a86ef49
Ben Ramsey <ben@benramsey.com>
2013-05-04 17:43:12 -0500

    Ensuring all copyright notices are alphabetized by last name.

--- src/Virtphp/Command/Helper/Help.php
+++ src/Virtphp/Command/Helper/Help.php
@@ -3,9 +3,9 @@
 /*
  * This file is part of VirtPHP.
  *
- * (c) Jordan Kasper <github @jakerella> 
- *     Jacques Woodcock <github @jwoodcock> 
+ * (c) Jordan Kasper <github @jakerella>
  *     Ben Ramsey <github @ramsey>
+ *     Jacques Woodcock <github @jwoodcock>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
