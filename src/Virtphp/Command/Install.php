58d7e4c7692469162095cfc81d2fa3947f589ae6
Jacques Woodcock <jacques@kitewebconsulting.com>
2013-05-03 22:08:41 -0500

    Remove vender added structure with blank files and gitignore

---
+++ src/Virtphp/Command/Install.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * This file is part of VirtPHP.
+ *
+ * (c) Jordan Kasper <github @jakerella> 
+ *     Jacques Woodcock <github @jwoodcock> 
+ *     Ben Ramsey <github @ramsey>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
