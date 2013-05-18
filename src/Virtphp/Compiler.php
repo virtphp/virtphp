59d0a38726e40872841c98f95018e81993a522a8
jakerella <jordan@jordankasper.com>
2013-05-18 14:59:12 -0500

    Scrubbed for coding standards

--- src/Virtphp/Compiler.php
+++ src/Virtphp/Compiler.php
@@ -27,24 +27,24 @@ class Compiler
      * @throws \RuntimeException
      * @param  string            $pharFile The full path to the file to create
      */
-    public function compile($pharFile = 'virtphp.phar')
+    public function compile($pharFile = "virtphp.phar")
     {
         if (file_exists($pharFile)) {
             unlink($pharFile);
         }
 
-        $process = new Process('git log --pretty="%H" -n1 HEAD', __DIR__);
+        $process = new Process("git log --pretty=\"%H\" -n1 HEAD", __DIR__);
         if ($process->run() != 0) {
-            throw new \RuntimeException('Can\'t run git log. You must ensure to run compile from virtphp git repository clone and that git binary is available.');
+            throw new \RuntimeException("Can\"t run git log. You must ensure to run compile from virtphp git repository clone and that git binary is available.");
         }
         $this->version = trim($process->getOutput());
 
-        $process = new Process('git describe --tags HEAD');
+        $process = new Process("git describe --tags HEAD");
         if ($process->run() == 0) {
             $this->version = trim($process->getOutput());
         }
 
-        $phar = new \Phar($pharFile, 0, 'virtphp.phar');
+        $phar = new \Phar($pharFile, 0, "virtphp.phar");
         $phar->setSignatureAlgorithm(\Phar::SHA1);
 
         $phar->startBuffering();
@@ -52,9 +52,9 @@ class Compiler
         $finder = new Finder();
         $finder->files()
             ->ignoreVCS(true)
-            ->name('*.php')
-            ->notName('Compiler.php')
-            ->in(__DIR__.'/..')
+            ->name("*.php")
+            ->notName("Compiler.php")
+            ->in(__DIR__."/..")
         ;
 
         foreach ($finder as $file) {
@@ -64,26 +64,26 @@ class Compiler
         $finder = new Finder();
         $finder->files()
             ->ignoreVCS(true)
-            ->name('*.php')
-            ->exclude('Tests')
-            ->in(__DIR__.'/../../vendor/symfony/')
+            ->name("*.php")
+            ->exclude("Tests")
+            ->in(__DIR__."/../../vendor/symfony/")
         ;
 
         foreach ($finder as $file) {
             $this->addFile($phar, $file);
         }
 
-        $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../res/php.ini'));
-        $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../res/activate.sh'));
+        $this->addFile($phar, new \SplFileInfo(__DIR__."/../../res/php.ini"));
+        $this->addFile($phar, new \SplFileInfo(__DIR__."/../../res/activate.sh"));
 
-        $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/autoload.php'));
-        $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/composer/autoload_namespaces.php'));
-        $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/composer/autoload_classmap.php'));
-        $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/composer/autoload_real.php'));
-        if (file_exists(__DIR__.'/../../vendor/composer/include_paths.php')) {
-            $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/composer/include_paths.php'));
+        $this->addFile($phar, new \SplFileInfo(__DIR__."/../../vendor/autoload.php"));
+        $this->addFile($phar, new \SplFileInfo(__DIR__."/../../vendor/composer/autoload_namespaces.php"));
+        $this->addFile($phar, new \SplFileInfo(__DIR__."/../../vendor/composer/autoload_classmap.php"));
+        $this->addFile($phar, new \SplFileInfo(__DIR__."/../../vendor/composer/autoload_real.php"));
+        if (file_exists(__DIR__."/../../vendor/composer/include_paths.php")) {
+            $this->addFile($phar, new \SplFileInfo(__DIR__."/../../vendor/composer/include_paths.php"));
         }
-        $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/composer/ClassLoader.php'));
+        $this->addFile($phar, new \SplFileInfo(__DIR__."/../../vendor/composer/ClassLoader.php"));
         $this->addVirtphpBin($phar);
 
         // Stubs
@@ -91,32 +91,32 @@ class Compiler
 
         $phar->stopBuffering();
 
-        $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../LICENSE'), false);
+        $this->addFile($phar, new \SplFileInfo(__DIR__."/../../LICENSE"), false);
 
         unset($phar);
     }
 
     private function addFile($phar, $file, $strip = true)
     {
-        $path = str_replace(dirname(dirname(__DIR__)).DIRECTORY_SEPARATOR, '', $file->getRealPath());
+        $path = str_replace(dirname(dirname(__DIR__)).DIRECTORY_SEPARATOR, "", $file->getRealPath());
 
         $content = file_get_contents($file);
         if ($strip) {
             $content = $this->stripWhitespace($content);
-        } elseif ('LICENSE' === basename($file)) {
+        } elseif ("LICENSE" === basename($file)) {
             $content = "\n".$content."\n";
         }
 
-        $content = str_replace('@package_version@', $this->version, $content);
+        $content = str_replace("@package_version@", $this->version, $content);
 
         $phar->addFromString($path, $content);
     }
 
     private function addVirtphpBin($phar)
     {
-        $content = file_get_contents(__DIR__.'/../../bin/virtphp');
-        $content = preg_replace('{^#!/usr/bin/env php\s*}', '', $content);
-        $phar->addFromString('bin/virtphp', $content);
+        $content = file_get_contents(__DIR__."/../../bin/virtphp");
+        $content = preg_replace('{^#!/usr/bin/env php\s*}', "", $content);
+        $phar->addFromString("bin/virtphp", $content);
     }
 
     /**
@@ -127,11 +127,11 @@ class Compiler
      */
     private function stripWhitespace($source)
     {
-        if (!function_exists('token_get_all')) {
+        if (!function_exists("token_get_all")) {
             return $source;
         }
 
-        $output = '';
+        $output = "";
         foreach (token_get_all($source) as $token) {
             if (is_string($token)) {
                 $output .= $token;
@@ -139,11 +139,11 @@ class Compiler
                 $output .= str_repeat("\n", substr_count($token[1], "\n"));
             } elseif (T_WHITESPACE === $token[0]) {
                 // reduce wide spaces
-                $whitespace = preg_replace('{[ \t]+}', ' ', $token[1]);
+                $whitespace = preg_replace("{[ \t]+}", " ", $token[1]);
                 // normalize newlines to \n
-                $whitespace = preg_replace('{(?:\r\n|\r|\n)}', "\n", $whitespace);
+                $whitespace = preg_replace("{(?:\r\n|\r|\n)}", "\n", $whitespace);
                 // trim leading spaces
-                $whitespace = preg_replace('{\n +}', "\n", $whitespace);
+                $whitespace = preg_replace("{\n +}", "\n", $whitespace);
                 $output .= $whitespace;
             } else {
                 $output .= $token[1];
@@ -155,7 +155,7 @@ class Compiler
 
     private function getStub()
     {
-        $stub = <<<'EOF'
+        $stub = <<<"EOF"
 #!/usr/bin/env php
 <?php
 /*
@@ -169,18 +169,18 @@ class Compiler
  * file that was distributed with this source code.
  */
 
-Phar::mapPhar('virtphp.phar');
+Phar::mapPhar("virtphp.phar");
 
 EOF;
 
         // add warning once the phar is older than 30 days
-        if (preg_match('{^[a-f0-9]+$}', $this->version)) {
+        if (preg_match("{^[a-f0-9]+$}", $this->version)) {
             $warningTime = time() + 30*86400;
-            $stub .= "define('VIRTPHP_DEV_WARNING_TIME', $warningTime);\n";
+            $stub .= "define(\"VIRTPHP_DEV_WARNING_TIME\", $warningTime);\n";
         }
 
-        return $stub . <<<'EOF'
-require 'phar://virtphp.phar/bin/virtphp';
+        return $stub . <<<"EOF"
+require "phar://virtphp.phar/bin/virtphp";
 
 __HALT_COMPILER();
 EOF;
