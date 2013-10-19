6565c017e98b84d3183e5c34e607bef230baab07
Ben Ramsey <ben@benramsey.com>
2013-10-19 16:03:58 +0000

    Add a Vagrantfile and manifests to quickly start a dev environment

---
+++ manifests/init.pp
@@ -0,0 +1,35 @@
+Exec {
+    path => [
+        "/usr/local/sbin",
+        "/usr/local/bin",
+        "/usr/sbin",
+        "/usr/bin",
+        "/sbin",
+        "/bin",
+        "/opt/vagrant_ruby/bin",
+    ],
+}
+
+exec { "Update Apt":
+    command => "apt-get update",
+}
+
+package { [
+        "build-essential",
+        "vim",
+        "git-core",
+        "curl",
+        "libcurl3-openssl-dev",
+        "php5-cli",
+        "php5-dev",
+        "php5-curl",
+        "php-pear",
+    ]:
+    ensure => "installed",
+}
+
+file { "/etc/php5/conf.d/phar.ini":
+    ensure => "present",
+    content => "phar.readonly = Off\n",
+    mode => 644,
+}
