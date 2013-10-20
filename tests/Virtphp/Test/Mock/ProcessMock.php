edaa44f00c37579c32f1796097e121452831f106
Ben Ramsey <ben@benramsey.com>
2013-10-20 22:09:40 +0000

    Update the ProcessMock to support a run() return value

--- tests/Virtphp/Test/Mock/ProcessMock.php
+++ tests/Virtphp/Test/Mock/ProcessMock.php
@@ -5,16 +5,18 @@ class ProcessMock
 {
     public $command;
     public $output = false;
+    public $runReturn = 0;
 
-    public function __construct($command, $output = false)
+    public function __construct($command, $output = false, $runReturn = 0)
     {
         $this->command = $command;
         $this->output = $output;
+        $this->runReturn = $runReturn;
     }
 
     public function run()
     {
-        return true;
+        return $this->runReturn;
     }
 
     public function getOutput()
