ad483ee9724fd7b2b916f8b9f1ff7ab6212ddde8
Jacques Woodcock <jacques@kitewebconsulting.com>
2014-03-19 22:50:01 -0500

    Finished valid directory check and throwing warnings. Also formatting fixes.

--- src/Virtphp/Workers/Shower.php
+++ src/Virtphp/Workers/Shower.php
@@ -76,26 +76,59 @@ class Shower extends AbstractWorker
         if ($this->getFilesystem()->exists($this->filePath)) {
 
             // get list of environments and convert to array
-            $envList = json_decode($this->getFileSystem()->getContents($this->filePath), true);
+            $envList = json_decode(
+                $this->getFileSystem()->getContents($this->filePath),
+                true
+            );
             $tableValues = array();
+            $envWarnings = array();
 
             // convert to table formatted rows
             foreach ($envList as $key => $value) {
+                // get path to source
+                $sourcePath = $value['path']
+                    . DIRECTORY_SEPARATOR
+                    . $key
+                    . DIRECTORY_SEPARATOR
+                    . 'bin'
+                    . DIRECTORY_SEPARATOR
+                    . 'activate';
+                $warning = '';
+                // verify if path is real
+                if (!$this->getFilesystem()->realpath($sourcePath)) {
+                    $warning = '(!)';
+                    $envWarnings[] = $key;
+                }
                 $tableValues[] = array(
-                    'name' => $key,
+                    'name' => $warning . $key,
                     'path' => $value['path'],
+                    'source' => 'source ' . $sourcePath
                 );
             }
 
             // build table
-            $this->getTableHelper()->setHeaders(array('Name', 'Path'))->setRows($tableValues);
+            $this->getTableHelper()->setHeaders(
+                array('Name', 'Path', 'ACTIVATE')
+            )->setRows($tableValues);
             // render table
             $this->getTableHelper()->render($this->output);
 
+            if (count($envWarnings) > 0) {
+                for ($x = 0; $x < count($envWarnings); $x++) {
+                    $this->output->writeln('');
+                    $this->output->writeln(
+                        "<comment>WARNING: The `" . $envWarnings[$x] . "`"
+                        . " virtPHP environment can no longer be found. Perhaps"
+                        . " it has moved or has been deleted.</comment>"
+                    );
+                    $this->output->writeln('');
+                }
+            }
+
         } else {
             $this->output->writeln(
-                "<error>either no environments have been created on this system or"
-                . " the json file has been moved</error>"
+                "<error>either no environments have been created on this system"
+                . " or the json file has been moved</error>"
             );
             return false;
         }
@@ -104,8 +137,8 @@ class Shower extends AbstractWorker
     }
 
     /**
-     * Method that takes a provided environment name and path and resyncs the record
-     * to the udpated path. 
+     * Method that takes a provided environment name and path and resyncs the
+     * record to the udpated path.
      *
      * @return boolean Weather or not the action was successful
      */
@@ -122,21 +155,32 @@ class Shower extends AbstractWorker
         }
 
         // get list of environments and convert to array
-        $envList = json_decode($this->getFileSystem()->getContents($this->filePath), true);
+        $envList = json_decode($this->getFileSystem()->getContents(
+            $this->filePath),
+            true
+        );
 
         if (!isset($envList[$envName])) {
             $this->output->writeln(
-                '<error>' . $envName . ' was not found as a valid virtPHP environment.</error>'
+                '<error>' . $envName . ' was not found as a valid virtPHP'
+                . ' environment.</error>'
             );
             return false;
         }
 
         $envList[$envName]['path'] = $realPath;
 
-        $this->getFilesystem()->dumpFile($this->filePath, json_encode($envList));
+        $this->getFilesystem()->dumpFile(
+            $this->filePath,
+            json_encode($envList)
+        );
 
         $this->output->writeln(
-            '<bg=green;options=bold>' . $envName . ' now has the path of '. $updatedPath . '</bg=green;options=bold>'
+            '<bg=green;options=bold>'
+            . $envName
+            . ' now has the path of '
+            . $updatedPath
+            . '</bg=green;options=bold>'
         );
         return true;
     }
