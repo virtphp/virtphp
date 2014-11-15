66498bc1386f6e818598aaa81a30b9a4de881065
Jacques Woodcock <jacques@kitewebconsulting.com>
2014-11-15 14:28:15 -0600

    Adding in all the tests.

--- tests/Virtphp/Test/Workers/ActivatorTest.php
+++ tests/Virtphp/Test/Workers/ActivatorTest.php
@@ -17,17 +17,19 @@ use Virtphp\TestCase;
 use Virtphp\TestOutput;
 use Virtphp\Test\Mock\FilesystemMock;
 use Virtphp\Util\Filesystem;
-use Virtphp\Workers\Shower;
+use Virtphp\Workers\Activator;
 use Virtphp\Test\Mock\TableMock;
+use Virtphp\Util\EnvironmentFile;
 
 class ActivatorTest extends TestCase
 {
     protected $testClonedEnv = 'tests/testClonedEnv';
     protected $testOriginalEnv = 'tests/testOriginalEnv';
-    protected $shower;
+    protected $activator;
     protected $output;
     protected $filesystemMock;
     protected $fs;
+    protected $envFile; 
 
     protected function setUp()
     {
@@ -41,17 +43,38 @@ class ActivatorTest extends TestCase
             'Virtphp\Test\Mock\FilesystemMock',
             array('getContents', 'exists')
         );
+        $this->envFile = $this->getMock(
+            'Virtphp\Util\EnvironmentFile',
+            array('getFileSystem', 'getEnvironments', 'checkForEnvironment', 'createEnvironmentsFile', 'addEnv', 'removeEnvFromList'),
+            array($this->output),
+            '',
+            false
+        );
 
-        $this->shower = $this->getMockBuilder('Virtphp\Workers\Shower')
-            ->setConstructorArgs(array($this->output))
-            ->setMethods(array('getFilesystem'))
+        $this->envFile->envContents = array(
+            'envName' => array(
+                'name' => 'envName',
+                'path' => '\/Users\/Kite\/.virtphp\/envs',
+            )
+        );
+
+        $this->envFile->expects($this->any())
+            ->method('checkForEnvironment')
+            ->will($this->returnValue(''));
+
+        $this->activator = $this->getMockBuilder('Virtphp\Workers\Activator')
+            ->setConstructorArgs(array($this->output, 'envName', $this->envFile))
+            ->setMethods(array('getFilesystem', 'getOs', 'copyToClipboard'))
             ->getMock();
-        $this->shower->expects($this->any())
+        $this->activator->expects($this->any())
             ->method('getFilesystem')
             ->will($this->returnValue($this->filesystemMock));
-        $this->shower->expects($this->any())
-            ->method('getTableHelper')
-            ->will($this->returnValue(new TableMock()));
+        $this->activator->expects($this->any())
+            ->method('getOs')
+            ->will($this->returnValue(true));
+        $this->activator->expects($this->any())
+            ->method('copyToClipboard')
+            ->will($this->returnValue(true));
     }
 
     protected function tearDown()
@@ -61,46 +84,36 @@ class ActivatorTest extends TestCase
     }
 
     /**
-     * @covers Virtphp\Workers\Shower::__construct
+     * @covers Virtphp\Workers\Activator::__construct
      */
     public function testConstruct()
     {
-        $shower = new Shower($this->output);
-        $shower->setTableHelper(new TableMock);
+        $activator = new Activator($this->output, 'envName', $this->envFile);
 
-        $this->assertInstanceOf('Virtphp\Workers\Shower', $shower);
-        $this->assertInstanceOf(
-            'Virtphp\Test\Mock\TableMock',
-            $shower->getTableHelper()
-        );
+        $this->assertInstanceOf('Virtphp\Workers\Activator', $activator);
+        // for some reason assertNotEquals was not found.
+        $this->assertNotEquals(false, $activator->getOs());
+        $this->assertFalse(false, $activator->copyToClipboard('TestEnv'));
     }
 
     /**
-     * @covers Virtphp\Workers\Shower::execute
-     * @covers Virtphp\Workers\Shower::setTableHelper
-     * @covers Virtphp\Workers\Shower::getTableHelper
+     * @covers Virtphp\Workers\Activator::execute
      */
     public function testExecute()
     {
 
         $this->filesystemMock->expects($this->any())
-            ->method('getContents')
-            ->will($this->returnValue(
-                '{"mytest":{"name":"mytest","path":"\/Users\/virtPHP\/work\/virtphp"}}'
-            ));
-        $this->filesystemMock->expects($this->any())
             ->method('exists')
             ->will($this->returnValue(true));
 
-        $this->shower->setTableHelper(new TableMock());
-        $this->assertTrue($this->shower->execute());
+        $this->assertTrue($this->activator->execute());
         $this->assertCount(1, $this->output->messages);
     }
 
     /**
-     * @covers Virtphp\Workers\Shower::execute
+     * @covers Virtphp\Workers\Activator::execute
      */
-    public function testExecuteFailedRealPath()
+    /*public function testExecuteFailedRealPath()
     {
         $filesystemMock = $this->getMock(
             'Virtphp\Test\Mock\FilesystemMock',
@@ -121,22 +134,22 @@ class ActivatorTest extends TestCase
                 . '"myenv":{"name":"myenv","path":"\/users\/Kite\/work\/theKit"}}'
             ));
 
-        $shower = $this->getMockBuilder('Virtphp\Workers\Shower')
+        $activator = $this->getMockBuilder('Virtphp\Workers\Activator')
             ->setConstructorArgs(array($this->output))
             ->setMethods(array('getFilesystem'))
             ->getMock();
-        $shower->expects($this->any())
+        $activator->expects($this->any())
             ->method('getFilesystem')
             ->will($this->returnValue($filesystemMock));
 
-        $this->assertTrue($shower->execute());
+        $this->assertTrue($activator->execute());
         $this->assertNotCount(0, $this->output->messages);
-    }
+    }*/
 
     /**
-     * @covers Virtphp\Workers\Shower::execute
+     * @covers Virtphp\Workers\Activator::execute
      */
-    public function testExecuteExceptionReturnsFalse()
+    /*public function testExecuteExceptionReturnsFalse()
     {
         $filesystemMock = $this->getMock(
             'Virtphp\Test\Mock\FilesystemMock',
@@ -146,22 +159,22 @@ class ActivatorTest extends TestCase
             ->method('exists')
             ->will($this->returnValue(false));
 
-        $shower = $this->getMockBuilder('Virtphp\Workers\Shower')
+        $activator = $this->getMockBuilder('Virtphp\Workers\Activator')
             ->setConstructorArgs(array($this->output))
             ->setMethods(array('getFilesystem'))
             ->getMock();
-        $shower->expects($this->any())
+        $activator->expects($this->any())
             ->method('getFilesystem')
             ->will($this->returnValue($filesystemMock));
 
-        $this->assertFalse($shower->execute());
+        $this->assertFalse($activator->execute());
         $this->assertNotCount(0, $this->output->messages);
-    }
+    }*/
 
     /**
-     * @covers Virtphp\Workers\Shower::updatePath
+     * @covers Virtphp\Workers\Activator::updatePath
      */
-    public function testUpdatePath()
+    /*public function testUpdatePath()
     {
 
         $this->filesystemMock->expects($this->any())
@@ -176,19 +189,19 @@ class ActivatorTest extends TestCase
         $target = 'mytest';
         $newPath = '\/Users\/Documents\/virtphp';
 
-        $this->shower->setTableHelper(new TableMock());
-        $this->assertTrue($this->shower->updatePath($target, $newPath));
+        $this->activator->setTableHelper(new TableMock());
+        $this->assertTrue($this->activator->updatePath($target, $newPath));
         $this->assertCount(1, $this->output->messages);
         $this->assertEquals(
             $target .' now has the path of ' . $newPath,
             $this->output->messages[0]
         );
-    }
+    }*/
 
     /**
-     * @covers Virtphp\Workers\Shower::updatePath
+     * @covers Virtphp\Workers\Activator::updatePath
      */
-    public function testUpdatePathFail()
+    /*public function testUpdatePathFail()
     {
 
         $this->filesystemMock->expects($this->any())
@@ -203,19 +216,19 @@ class ActivatorTest extends TestCase
         $target = 'noRecord';
         $newPath = '\/Users\/Documents\/virtphp';
 
-        $this->shower->setTableHelper(new TableMock());
-        $this->assertFalse($this->shower->updatePath($target, $newPath));
+        $this->activator->setTableHelper(new TableMock());
+        $this->assertFalse($this->activator->updatePath($target, $newPath));
         $this->assertCount(1, $this->output->messages);
         $this->assertEquals(
             $target .' was not found as a valid virtPHP environment.',
             $this->output->messages[0]
         );
-    }
+    }*/
 
     /**
-     * @covers Virtphp\Workers\Shower::updatePath
+     * @covers Virtphp\Workers\Activator::updatePath
      */
-    public function testUpdatePathBadPath()
+    /*public function testUpdatePathBadPath()
     {
 
         $this->filesystemMock->expects($this->any())
@@ -233,12 +246,12 @@ class ActivatorTest extends TestCase
         $target = 'noRecord';
         $newPath = '';
 
-        $this->shower->setTableHelper(new TableMock());
-        $this->assertFalse($this->shower->updatePath($target, $newPath));
+        $this->activator->setTableHelper(new TableMock());
+        $this->assertFalse($this->activator->updatePath($target, $newPath));
         $this->assertCount(1, $this->output->messages);
         $this->assertEquals(
             'Path provided is not an actual path.',
             $this->output->messages[0]
         );
-    }
+    }*/
 }
